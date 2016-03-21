8de99eed99eb63a2e2040cba5fc143acb07992de
trikitrok <trikitrok@gmail.com>
2016-03-22 00:07:05 +0100

    Added sugar for rolling all dice

--- spec/unit/dice_roller_spec.rb
+++ spec/unit/dice_roller_spec.rb
@@ -1,12 +1,12 @@
 require_relative "../../dice_roller"
 
 describe "Dice rolling" do
-  it "rolls the given dice" do
+  it "rolls all the dice" do
     die_roller = double()
     allow(die_roller).to receive(:roll).and_return(1,2,3,4,5)
     dice_roller = DiceRoller.new(die_roller)
 
-    dice_roller.roll([:d1, :d2, :d3, :d4, :d5])
+    dice_roller.roll_all()
 
     expect(dice_roller.last_rolled_dice).to eq({d1: 1, d2: 2, d3: 3, d4: 4, d5: 5})
   end
