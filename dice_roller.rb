6cb5ffa775b65b1b91eea24ad3a91e8b3a67bddc
trikitrok <trikitrok@gmail.com>
2016-03-22 00:11:25 +0100

    DiceRoller rolls the desired dice keeping the previous value of the other ones

--- dice_roller.rb
+++ dice_roller.rb
@@ -9,7 +9,7 @@ class DiceRoller
   end
 
   def roll dice_to_roll
-    @last_rolled_dice = dice_to_roll.reduce({}) do |acc, die|
+    @last_rolled_dice = dice_to_roll.reduce(@last_rolled_dice) do |acc, die|
       acc[die] = @die_roller.roll()
       acc
     end
--- spec/unit/dice_roller_spec.rb
+++ spec/unit/dice_roller_spec.rb
@@ -10,4 +10,16 @@ describe "Dice rolling" do
 
     expect(dice_roller.last_rolled_dice).to eq({d1: 1, d2: 2, d3: 3, d4: 4, d5: 5})
   end
+
+  it "rolls only the desired dice keeping the prvious value of the other ones" do
+    die_roller = double()
+    allow(die_roller).to receive(:roll).and_return(1,2,3,4,5,
+                                                   4, 1)
+    dice_roller = DiceRoller.new(die_roller)
+    dice_roller.roll_all()
+
+    dice_roller.roll([:d3, :d5])
+
+    expect(dice_roller.last_rolled_dice).to eq({d1: 1, d2: 2, d3: 4, d4: 4, d5: 1})
+  end
 end
