c293c11a665f6ea4a4aa1de8173e76142ca14f04
trikitrok <trikitrok@gmail.com>
2016-03-22 00:15:06 +0100

    Removed some duplication

--- spec/unit/dice_roller_spec.rb
+++ spec/unit/dice_roller_spec.rb
@@ -2,9 +2,7 @@ require_relative "../../dice_roller"
 
 describe "Dice rolling" do
   it "rolls all the dice" do
-    die_roller = double()
-    allow(die_roller).to receive(:roll).and_return(1,2,3,4,5)
-    dice_roller = DiceRoller.new(die_roller)
+    dice_roller = a_dice_roller_rolling_values(1,2,3,4,5)
 
     dice_roller.roll_all()
 
@@ -12,14 +10,18 @@ describe "Dice rolling" do
   end
 
   it "rolls only the desired dice keeping the prvious value of the other ones" do
-    die_roller = double()
-    allow(die_roller).to receive(:roll).and_return(1,2,3,4,5,
-                                                   4, 1)
-    dice_roller = DiceRoller.new(die_roller)
+    dice_roller = dice_roller = a_dice_roller_rolling_values(1,2,3,4,5,
+                                                                 4,1)
     dice_roller.roll_all()
-
+    
     dice_roller.roll([:d3, :d5])
 
     expect(dice_roller.last_rolled_dice).to eq({d1: 1, d2: 2, d3: 4, d4: 4, d5: 1})
+  end
+
+  def a_dice_roller_rolling_values *values
+    die_roller = double()
+    allow(die_roller).to receive(:roll).and_return(*values)
+    DiceRoller.new(die_roller)
   end
 end
