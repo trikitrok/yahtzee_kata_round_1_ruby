7304aadf206d6cc31dc3c51e50b72af086cee78f
trikitrok <trikitrok@gmail.com>
2016-03-22 00:30:23 +0100

    Extracted heolper

--- yahtzee.rb
+++ yahtzee.rb
@@ -10,15 +10,19 @@ class Yahtzee
     @console.print("Category: Ones")
     roll([:d1, :d2, :d3, :d4, :d5])
     @console.print("[1] Dice to re-run:")
-    user_input = @user_input_reader.read_line()
-    dice_to_rerun = user_input.split(" ").map{|die_as_str| die_as_str.downcase.to_sym }
     roll(dice_to_rerun)
   end
 
   private
+
   def roll dice_to_roll
     @dice_roller.roll(dice_to_roll)
     rolled_dice = @dice_roller.last_rolled_dice()
     @notifier.notify_rolled_dice(rolled_dice.values())
+  end
+
+  def dice_to_rerun
+    user_input = @user_input_reader.read_line()
+    user_input.split(" ").map{|die_as_str| die_as_str.downcase.to_sym }
   end
 end
