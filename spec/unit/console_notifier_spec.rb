73669f19ca06bdddd429c814a0ecaecc9ecfd160
trikitrok <trikitrok@gmail.com>
2016-03-22 00:55:22 +0100

    Removed some duplication

--- spec/unit/console_notifier_spec.rb
+++ spec/unit/console_notifier_spec.rb
@@ -1,18 +1,16 @@
 require_relative "../../console_notifier"
 
 describe "Console Notifier" do
-  it "notifies the rolled dice" do
-    console = FakeConsole.new
-    notifier = ConsoleNotifier.new(console)
+  let(:console) { FakeConsole.new }
+  let(:notifier) { ConsoleNotifier.new(console) }
 
+  it "notifies the rolled dice" do
     notifier.notify_rolled_dice([2, 4, 1, 6, 1])
 
     expect(console.output).to eq(["Dice: D1:2 D2:4 D3:1 D4:6 D5:1"])
   end
 
   it "notifies the user to introduce which dice to rerun" do
-    console = FakeConsole.new
-    notifier = ConsoleNotifier.new(console)
     reruns_so_far = 1
 
     notifier.notify_user_to_introduce_dice_to_rerun(reruns_so_far)
