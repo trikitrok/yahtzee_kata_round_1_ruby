f207ac37a4e72da06bea9df15849ba1791b7bc76
trikitrok <trikitrok@gmail.com>
2016-03-21 23:24:23 +0100

    Extracted helper method

--- yahtzee.rb
+++ yahtzee.rb
@@ -6,8 +6,12 @@ class Yahtzee
 
   def play
     @console.print("Category: Ones")
-    rolled_dice = []
-    5.times { rolled_dice << @die_roller.roll() }
+    roll([:d1, :d2, :d3, :d4, :d5])
+  end
+
+  private 
+  def roll dice
+    rolled_dice = dice.map { @die_roller.roll() }
     @console.print("Dice: D1:#{rolled_dice[0]} D2:#{rolled_dice[1]} D3:#{rolled_dice[2]} D4:#{rolled_dice[3]} D5:#{rolled_dice[4]}")
   end
 end
