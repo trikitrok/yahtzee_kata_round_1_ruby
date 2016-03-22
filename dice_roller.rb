312a5ef71ea23a2d70e4edce58af6638c096bb3e
trikitrok <trikitrok@gmail.com>
2016-03-22 04:03:40 +0100

    Removed unused code

--- dice_roller.rb
+++ dice_roller.rb
@@ -4,10 +4,6 @@ class DiceRoller
     @last_rolled_dice = {}
   end
 
-  def roll_all
-    roll([:d1, :d2, :d3, :d4, :d5])
-  end
-
   def roll dice_to_roll
     @last_rolled_dice = dice_to_roll.reduce(@last_rolled_dice) do |acc, die|
       acc[die] = @die_roller.roll()
--- spec/unit/dice_roller_spec.rb
+++ spec/unit/dice_roller_spec.rb
@@ -4,7 +4,7 @@ describe "Dice rolling" do
   it "rolls all the dice" do
     dice_roller = a_dice_roller_rolling_values(1,2,3,4,5)
 
-    dice_roller.roll_all()
+    dice_roller.roll([:d1, :d2, :d3, :d4, :d5])
 
     expect(dice_roller.last_rolled_dice).to eq({d1: 1, d2: 2, d3: 3, d4: 4, d5: 5})
   end
@@ -12,7 +12,7 @@ describe "Dice rolling" do
   it "rolls only the desired dice keeping the prvious value of the other ones" do
     dice_roller = dice_roller = a_dice_roller_rolling_values(1,2,3,4,5,
                                                                  4,1)
-    dice_roller.roll_all()
+    dice_roller.roll([:d1, :d2, :d3, :d4, :d5])
     
     dice_roller.roll([:d3, :d5])
 
