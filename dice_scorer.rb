665fae927c5d7f87e702eb82d93e8471861ca082
trikitrok <trikitrok@gmail.com>
2016-03-22 01:10:31 +0100

    Created DiceScorer and computing score for Ones category by faking it

---
+++ dice_scorer.rb
@@ -0,0 +1,13 @@
+class DiceScorer
+  def self.for_category(category)
+    new(category)
+  end
+
+  def initialize category
+
+  end
+
+  def compute_score dice
+    2
+  end
+end
---
+++ spec/unit/dice_score_spec.rb
@@ -0,0 +1,10 @@
+require_relative "../../dice_scorer"
+
+describe "Scoring dice by category" do
+  it "computes the dice score for Ones" do
+    scorer = DiceScorer.for_category(:ones)
+
+    expect(scorer.compute_score({d1: 1, d2: 1, d3:4, d4: 2, d5: 6})).to eq(2)
+  end
+  
+end
